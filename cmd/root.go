aa68da2716d32c0e9a9b0b263d2b67e626f40e5c
Aman Verma <verma1090aman@gmail.com>
2022-07-17 20:05:14 +0530

    fix: add license headers and update existing ones.

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2022 Aman Verma. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
 */
+
 package cmd
 
 import (
