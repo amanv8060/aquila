bc02239bc1a26d6b2ea6482785dfbfc2bcaaeda9
Aman Verma <verma1090aman@gmail.com>
2023-01-18 04:01:08 +0530

    Add inplace replace for existing snippet.
    add basic check for the snippet to be existing beforehand, if the snipped already exists, replace the existing one.

--- utils/updateRegions.go
+++ utils/updateRegions.go
@@ -4,6 +4,7 @@ import (
 	"aquila/models"
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"io"
@@ -12,12 +13,11 @@ import (
 	"strings"
 )
 
-var docsPath = viper.GetString("docsPath")
-
 func UpdateRegions() {
 
 	// recursively walk through docs directory
 	// for each file, read it and update the code regions
+	var docsPath = viper.GetString("docsPath")
 
 	// walk through the docs directory
 	// for each file, read it and update the code regions
@@ -26,7 +26,7 @@ func UpdateRegions() {
 			return err
 		}
 		if !info.IsDir() {
-			// fmt.Println("reading file: ", path)
+			fmt.Println("reading file: ", path)
 			// read the file and update the code regions
 			processFile(path)
 		}
@@ -62,15 +62,42 @@ func processFile(path string) {
 	// flag to check if the code region was inserted or not
 	var codeRegionInserted bool
 
-	// loop over the lines
-	for _, line := range lines {
-		// update codeRegionInserted
-		wasUpdated := processWriteLine(line, &updatedLines)
+	// indexed loop over the lines
+	var linesLen = len(lines)
+	for i := 0; i < linesLen; i++ {
+		line := lines[i]
 
-		if !codeRegionInserted {
-			codeRegionInserted = wasUpdated
+		// add the line to the updatedLines array
+		updatedLines = append(updatedLines, line)
+
+		// find if the current line has regex in it.
+		codeRegion := models.GetCodeRegion(line)
+
+		if codeRegion != nil {
+			var wasUpdated = processWriteLine(codeRegion, &updatedLines)
+
+			// check if this is the last line
+			if i <= linesLen-1 {
+				nextLine := lines[i+1]
+				// if this is not the last line, check if the next starts with code block.
+				if strings.HasPrefix(nextLine, "```") {
+					//loop over until we find the next line that ends the code block
+					for j := i + 2; j < linesLen; j++ {
+						if strings.HasPrefix(lines[j], "```") {
+							// break the loop
+							i = j
+							break
+						}
+					}
+				}
+			}
+
+			if !codeRegionInserted {
+				codeRegionInserted = wasUpdated
+			}
 		}
 	}
+
 	// rewrite the file if code regions were inserted
 	if codeRegionInserted {
 		// delete the contents of the file
@@ -96,68 +123,59 @@ func processFile(path string) {
 
 // function to read the lines, and update the newlines array, which is passed by reference.
 // returns a bool indicating whether the code region was inserted or not
-func processWriteLine(line string, updatedLines *[]string) bool {
-	// match line with regex
+func processWriteLine(codeRegion *models.CodeRegion, updatedLines *[]string) bool {
 
-	codeRegion := models.GetCodeRegion(line)
+	// fetch code region from yaml file
+	splitPath := strings.Split(codeRegion.Path, ".")
+	_, extension := splitPath[0], splitPath[1]
 
-	// insert the current line into the updatedLines array
-	*updatedLines = append(*updatedLines, line)
+	jsonPath := ExcerptsPath + codeRegion.Path + ".json"
 
-	if codeRegion != nil {
-		// fetch code region from yaml file
-		splitPath := strings.Split(codeRegion.Path, ".")
-		modifiedCodeRegionPath, extension := splitPath[0], splitPath[1]
-
-		jsonPath := ExcerptsPath + modifiedCodeRegionPath + ".json"
-
-		// read the yaml file
-		jsonFile, err := os.OpenFile(jsonPath, os.O_RDONLY, os.ModePerm)
-		if err != nil {
-			log.Fatal().Msgf("open file error: %v", err)
-			return false
-		}
-
-		defer func(yamlFile *os.File) {
-			err := yamlFile.Close()
-			if err != nil {
-				log.Fatal().Msgf("close file error: %v", err)
-			}
-		}(jsonFile)
-
-		var regions = make(map[string][]string)
-
-		decoder := json.NewDecoder(jsonFile)
-
-		// read the json file
-		err = decoder.Decode(&regions)
-		if err != nil {
-			return false
-		}
-
-		// get the code region from the yaml file
-		codeRegionLines := regions[codeRegion.RegionName]
-
-		if err != nil {
-			log.Fatal().Msgf("write to file error: %v", err)
-			return false
-		}
-		header := "```" + extension
-		footer := "```"
-		// if codeRegionLines are not empty or nil, insert the code region
-		if codeRegionLines != nil && len(codeRegionLines) > 0 {
-			// insert header
-			*updatedLines = append(*updatedLines, header)
-
-			// insert code region lines
-			*updatedLines = append(*updatedLines, codeRegionLines...)
-
-			// insert footer
-			*updatedLines = append(*updatedLines, footer)
-
-			return true
-		}
+	// read the yaml file
+	jsonFile, err := os.OpenFile(jsonPath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatal().Msgf("open file error: %v", err)
 		return false
+	}
+
+	defer func(yamlFile *os.File) {
+		err := yamlFile.Close()
+		if err != nil {
+			log.Fatal().Msgf("close file error: %v", err)
+		}
+	}(jsonFile)
+
+	var regions = make(map[string][]string)
+
+	decoder := json.NewDecoder(jsonFile)
+
+	// read the json file
+	err = decoder.Decode(&regions)
+	if err != nil {
+		return false
+	}
+
+	// get the code region from the yaml file
+	codeRegionLines := regions[codeRegion.RegionName]
+
+	if err != nil {
+		log.Fatal().Msgf("write to file error: %v", err)
+		return false
+	}
+	header := "```" + extension
+	footer := "```"
+	// if codeRegionLines are not empty or nil, insert the code region
+	if codeRegionLines != nil && len(codeRegionLines) > 0 {
+		// insert header
+		*updatedLines = append(*updatedLines, header)
+
+		// insert code region lines
+		*updatedLines = append(*updatedLines, codeRegionLines...)
+
+		// insert footer
+		*updatedLines = append(*updatedLines, footer)
+
+		return true
 	}
 	return false
 }
